7af8c121ed7008ddc50b204824c8e6acdba8165f
meowoodie <woodielove@163.com>
2016-04-15 18:16:16 +0800

    revamp rbm and make it compatible with my own input dataset

--- data_generator.py
+++ data_generator.py
@@ -1,115 +1,70 @@
-from random import uniform, gauss, choice, randint
-from math import pow, sin
 import numpy as np
+import matplotlib.pyplot as plt
+from random import uniform, gauss, choice, randint
 
+# Global variables
+noise_sigma = 100
+sin_lam_max = 0.1
+sin_bias_max = 1000
 
-class DataGenerator():
-    func = ("uniform", "linear", "power", "sin")
+# Kernel functions
+def poisson_seq(n=10, lam=100, noise_k=1):
+    seq = np.random.poisson(lam, n)
+    return np.array(map(lambda x: abs(int(x + noise_k * (np.random.normal(lam, noise_sigma) - lam))), seq))
 
-    def __init__(self, len, count):
-        self.len = len
-        self.count = count
+def sin_seq(n=10, lam=2, phase=5, bias=0, K=100, noise_k=1):
+    _x = np.linspace((0 + phase) * lam, (n + phase) * lam, n)
+    seq = np.sin(_x) + 1
+    return np.array(map(lambda x: abs(int(K * x + bias + noise_k * (np.random.normal(K, noise_sigma) - K))), seq))
 
-    def noise(self):
-        noise_avg = 0
-        noise_std = 1
-        return gauss(noise_avg, noise_std)
+def linear_seq():
+    pass
 
+# Every content sequence consist of one poisson component and several sin components
+def content_seq(n=10, lam=100, noise_k=1):
+    print "Content seq:"
+    print "----------------------------------------------------"
+    k_list = list(decomposition(lam))
+    print "The proportion of each component includes: ", k_list
+    pop_index = randint(0, len(k_list) - 1)
+    print "The proportion of poisson is: ", k_list[pop_index]
+    seq = poisson_seq(n=n, lam=k_list.pop(pop_index), noise_k=noise_k)
+    print "Poisson component is:\n ", seq
 
-    def uniform_function(self, x, noise_factor, K):
-        y = K * (1 + self.noise() * noise_factor)
-        if y < 0:
-            y = 0
-        return y
+    for k in k_list:
+        phase = uniform(-np.pi, np.pi)
+        sin_lam = uniform(0, sin_lam_max)
+        sin_bias = uniform(0, sin_bias_max)
+        new_seq = sin_seq(n=n, lam=sin_lam, phase=phase, bias=sin_bias, K=k, noise_k=noise_k)
+        seq += new_seq
+        print "Sin component is (proportion:[%d]):\n " % k, new_seq
 
-    def linear_function(self, x, noise_factor, K, bias, phase):
-        y = K * (1 + self.noise() * noise_factor) * (x - phase) + bias
-        if y < 0:
-            y = 0
-        return y
+    return seq
 
-    def power_function(self, x, noise_factor, K, bias, phase, exp):
-        y = K * (1 + self.noise() * noise_factor) * pow(x - phase, exp) + bias
-        if y < 0:
-            y = 0
-        return y
+# Utils
+def decomposition(i):
+    while i > 0:
+        n = randint(1, i)
+        yield n
+        i -= n
 
-    def sin_function(self, x, noise_factor, K, bias, phase):
-        y = K * (sin(x - phase) + self.noise() * noise_factor) + bias
-        if y < 0:
-            y = 0
-        return y
+def plot_graph(seqs, n=10):
+    x = np.linspace(0, n*10, n)
 
-    def signal_iterator(self, func_id):
-        signal = []
-        # The proportion of noise could not over 30% of signal.
-        noise_factor = uniform(0, 0.3)
-        # Factor of any kind of function, used to amplify or minify the signal.
-        K = uniform(-10, 10)
-        # Bias of any kind of function, used to shift the signal to up(increase) or down(decrease).
-        # The proportion of absolute value of bias could not over 100% of signal.
-        bias = randint(0, 5)
-        # Phase of any kind of function(Except uniform function), used to shift the signal to left(backward) or right(forward).
-        phase = uniform(-10, 10)
-        # Exponent of power funtion
-        exp = randint(-2, 2)
-        for x in range(0, self.len):
-            if func_id == 0:
-                y = self.uniform_function(x, noise_factor, abs(K))
-            elif func_id == 1:
-                y = self.linear_function(x, noise_factor, K, bias, phase)
-            elif func_id == 2:
-                y = self.power_function(x, noise_factor, K, bias, phase, exp)
-            elif func_id == 3:
-                y = self.sin_function(x, noise_factor, K, bias, phase)
-            else:
-                y = -1
-            signal.append(int(y))
-        return signal
+    # with plt.style.context('fivethirtyeight'):
+    for seq in seqs:
+        plt.plot(x, seq)
 
-    def generate(self):
-        dataset = []
-        func_info = []
-        func_ids = [0, 3, 3]
-        for i in range(0, self.count):
-            # func_id = choice([0, 3])
-            func_id = func_ids.pop()
-            func_info.append(self.func[func_id])
-            dataset.append(self.signal_iterator(func_id))
-        return dataset, func_info
-
-def generate_one_node_dataset(T, c_num):
-    dg = DataGenerator(T, c_num)
-    dataset, func = dg.generate()
-    return np.array(dataset).T
-
-def generate_multi_nodes_dataset(n_num, T, c_num):
-    dataset = []
-    for i in range(n_num):
-        dataset.append(generate_one_node_dataset(T, c_num))
-    return np.array(dataset)
-
-
-def binomial_function(mean):
-    return np.random.binomial(1, mean)
-
-def generate_multi_nodes_binomial(n_num, T, c_num, mean):
-    dataset = []
-    for i in range(n_num):
-        t_seq = []
-        for j in range(T):
-            c_set = []
-            for k in range(c_num):
-                c_set.append(binomial_function(mean))
-            t_seq.append(c_set)
-        dataset.append(t_seq)
-    return np.array(dataset)
+    plt.show()
 
 if __name__ == "__main__":
-    # dg = DataGenerator(10, 10)
-    # ds1, funcs1 = dg.generate()
-    # print funcs1
-    # for d in ds1:
-    #     print d
-    print generate_multi_nodes_binomial(2, 4, 88, 0.5)
-
+    seqs = []
+    n = 100
+    seqs.append(content_seq(n=n, lam=8000, noise_k=1))
+    # seqs.append(content_seq(n=n, lam=7000, noise_k=1))
+    # seqs.append(content_seq(n=n, lam=6000, noise_k=1))
+    # seqs.append(content_seq(n=n, lam=1000, noise_k=1))
+    # seqs.append(content_seq(n=n, lam=3000, noise_k=1))
+    # for i in range(5):
+    #     seqs.append(content_seq(n=n, lam=2000, noise_k=1))
+    plot_graph(seqs, n=n)
--- rbm.py
+++ rbm.py
@@ -6,10 +6,10 @@ to those without visible-visible and hidden-hidden connections.
 """
 import timeit
 
-try:
-    import PIL.Image as Image
-except ImportError:
-    import Image
+# try:
+#     import PIL.Image as Image
+# except ImportError:
+#     import Image
 
 import numpy
 
@@ -19,8 +19,8 @@ import os
 
 from theano.tensor.shared_randomstreams import RandomStreams
 
-from utils import tile_raster_images
-from logistic_sgd import load_data
+# from utils import tile_raster_images
+# from logistic_sgd import load_data
 
 
 # start-snippet-1
@@ -356,10 +356,10 @@ class RBM(object):
         return cross_entropy
 
 
-def test_rbm(learning_rate=0.1, training_epochs=15,
-             dataset='mnist.pkl.gz', batch_size=20,
-             n_chains=20, n_samples=10, output_folder='rbm_plots',
-             n_hidden=500):
+def test_rbm(learning_rate=0.02, training_epochs=150,
+             dataset='mnist.pkl.gz', batch_size=2,
+             n_chains=2, n_samples=10, output_folder='rbm_plots',
+             n_hidden=5):
     """
     Demonstrate how to train and afterwards sample from it using Theano.
 
@@ -378,17 +378,33 @@ def test_rbm(learning_rate=0.1, training_epochs=15,
     :param n_samples: number of samples to plot for each chain
 
     """
-    datasets = load_data(dataset)
+    dataset = [[[0, 1, 0],
+                [1, 0, 0],
+                [1, 1, 0],
+                [1, 1, 0],
+                [1, 1, 1],
+                [1, 1, 0]],
+               [[0, 1, 0],
+                [1, 0, 0],
+                [1, 1, 0],
+                [1, 1, 0],
+                [1, 1, 1],
+                [1, 1, 0]]]
 
-    train_set_x, train_set_y = datasets[0]
-    test_set_x, test_set_y = datasets[2]
+    datasets = numpy.array(dataset)
+    datasets = datasets.astype(theano.config.floatX)
+    # datasets = load_data(dataset)
+
+    train_set_x, train_set_y = datasets
+    train_set_x = theano.shared(train_set_x)
+    # test_set_x, test_set_y = datasets[2]
 
     # compute number of minibatches for training, validation and testing
     n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
 
     # allocate symbolic variables for the data
     index = T.lscalar()    # index to a [mini]batch
-    x = T.matrix('x')  # the data is presented as rasterized images
+    x = T.matrix('x')      # the data is presented as rasterized images
 
     rng = numpy.random.RandomState(123)
     theano_rng = RandomStreams(rng.randint(2 ** 30))
@@ -400,7 +416,7 @@ def test_rbm(learning_rate=0.1, training_epochs=15,
                                      borrow=True)
 
     # construct the RBM class
-    rbm = RBM(input=x, n_visible=28 * 28,
+    rbm = RBM(input=x, n_visible=3,
               n_hidden=n_hidden, numpy_rng=rng, theano_rng=theano_rng)
 
     # get the cost and the gradient corresponding to one step of CD-15
@@ -410,9 +426,9 @@ def test_rbm(learning_rate=0.1, training_epochs=15,
     #################################
     #     Training the RBM          #
     #################################
-    if not os.path.isdir(output_folder):
-        os.makedirs(output_folder)
-    os.chdir(output_folder)
+    # if not os.path.isdir(output_folder):
+    #     os.makedirs(output_folder)
+    # os.chdir(output_folder)
 
     # start-snippet-5
     # it is ok for a theano function to have no output
@@ -443,15 +459,15 @@ def test_rbm(learning_rate=0.1, training_epochs=15,
         # Plot filters after each training epoch
         plotting_start = timeit.default_timer()
         # Construct image from the weight matrix
-        image = Image.fromarray(
-            tile_raster_images(
-                X=rbm.W.get_value(borrow=True).T,
-                img_shape=(28, 28),
-                tile_shape=(10, 10),
-                tile_spacing=(1, 1)
-            )
-        )
-        image.save('filters_at_epoch_%i.png' % epoch)
+        # image = Image.fromarray(
+        #     tile_raster_images(
+        #         X=rbm.W.get_value(borrow=True).T,
+        #         img_shape=(28, 28),
+        #         tile_shape=(10, 10),
+        #         tile_spacing=(1, 1)
+        #     )
+        # )
+        # image.save('filters_at_epoch_%i.png' % epoch)
         plotting_stop = timeit.default_timer()
         plotting_time += (plotting_stop - plotting_start)
 
@@ -460,81 +476,83 @@ def test_rbm(learning_rate=0.1, training_epochs=15,
     pretraining_time = (end_time - start_time) - plotting_time
 
     print ('Training took %f minutes' % (pretraining_time / 60.))
+
     # end-snippet-5 start-snippet-6
     #################################
     #     Sampling from the RBM     #
     #################################
     # find out the number of test samples
-    number_of_test_samples = test_set_x.get_value(borrow=True).shape[0]
 
-    # pick random test examples, with which to initialize the persistent chain
-    test_idx = rng.randint(number_of_test_samples - n_chains)
-    persistent_vis_chain = theano.shared(
-        numpy.asarray(
-            test_set_x.get_value(borrow=True)[test_idx:test_idx + n_chains],
-            dtype=theano.config.floatX
-        )
-    )
-    # end-snippet-6 start-snippet-7
-    plot_every = 1000
-    # define one step of Gibbs sampling (mf = mean-field) define a
-    # function that does `plot_every` steps before returning the
-    # sample for plotting
-    (
-        [
-            presig_hids,
-            hid_mfs,
-            hid_samples,
-            presig_vis,
-            vis_mfs,
-            vis_samples
-        ],
-        updates
-    ) = theano.scan(
-        rbm.gibbs_vhv,
-        outputs_info=[None, None, None, None, None, persistent_vis_chain],
-        n_steps=plot_every
-    )
-
-    # add to updates the shared variable that takes care of our persistent
-    # chain :.
-    updates.update({persistent_vis_chain: vis_samples[-1]})
-    # construct the function that implements our persistent chain.
-    # we generate the "mean field" activations for plotting and the actual
-    # samples for reinitializing the state of our persistent chain
-    sample_fn = theano.function(
-        [],
-        [
-            vis_mfs[-1],
-            vis_samples[-1]
-        ],
-        updates=updates,
-        name='sample_fn'
-    )
-
-    # create a space to store the image for plotting ( we need to leave
-    # room for the tile_spacing as well)
-    image_data = numpy.zeros(
-        (29 * n_samples + 1, 29 * n_chains - 1),
-        dtype='uint8'
-    )
-    for idx in xrange(n_samples):
-        # generate `plot_every` intermediate samples that we discard,
-        # because successive samples in the chain are too correlated
-        vis_mf, vis_sample = sample_fn()
-        print ' ... plotting sample ', idx
-        image_data[29 * idx:29 * idx + 28, :] = tile_raster_images(
-            X=vis_mf,
-            img_shape=(28, 28),
-            tile_shape=(1, n_chains),
-            tile_spacing=(1, 1)
-        )
-
-    # construct image
-    image = Image.fromarray(image_data)
-    image.save('samples.png')
-    # end-snippet-7
-    os.chdir('../')
+    # number_of_test_samples = test_set_x.get_value(borrow=True).shape[0]
+    #
+    # # pick random test examples, with which to initialize the persistent chain
+    # test_idx = rng.randint(number_of_test_samples - n_chains)
+    # persistent_vis_chain = theano.shared(
+    #     numpy.asarray(
+    #         test_set_x.get_value(borrow=True)[test_idx:test_idx + n_chains],
+    #         dtype=theano.config.floatX
+    #     )
+    # )
+    # # end-snippet-6 start-snippet-7
+    # plot_every = 1000
+    # # define one step of Gibbs sampling (mf = mean-field) define a
+    # # function that does `plot_every` steps before returning the
+    # # sample for plotting
+    # (
+    #     [
+    #         presig_hids,
+    #         hid_mfs,
+    #         hid_samples,
+    #         presig_vis,
+    #         vis_mfs,
+    #         vis_samples
+    #     ],
+    #     updates
+    # ) = theano.scan(
+    #     rbm.gibbs_vhv,
+    #     outputs_info=[None, None, None, None, None, persistent_vis_chain],
+    #     n_steps=plot_every
+    # )
+    #
+    # # add to updates the shared variable that takes care of our persistent
+    # # chain :.
+    # updates.update({persistent_vis_chain: vis_samples[-1]})
+    # # construct the function that implements our persistent chain.
+    # # we generate the "mean field" activations for plotting and the actual
+    # # samples for reinitializing the state of our persistent chain
+    # sample_fn = theano.function(
+    #     [],
+    #     [
+    #         vis_mfs[-1],
+    #         vis_samples[-1]
+    #     ],
+    #     updates=updates,
+    #     name='sample_fn'
+    # )
+    #
+    # # create a space to store the image for plotting ( we need to leave
+    # # room for the tile_spacing as well)
+    # image_data = numpy.zeros(
+    #     (29 * n_samples + 1, 29 * n_chains - 1),
+    #     dtype='uint8'
+    # )
+    # for idx in xrange(n_samples):
+    #     # generate `plot_every` intermediate samples that we discard,
+    #     # because successive samples in the chain are too correlated
+    #     vis_mf, vis_sample = sample_fn()
+    #     print ' ... plotting sample ', idx
+    #     image_data[29 * idx:29 * idx + 28, :] = tile_raster_images(
+    #         X=vis_mf,
+    #         img_shape=(28, 28),
+    #         tile_shape=(1, n_chains),
+    #         tile_spacing=(1, 1)
+    #     )
+    #
+    # # construct image
+    # image = Image.fromarray(image_data)
+    # image.save('samples.png')
+    # # end-snippet-7
+    # os.chdir('../')
 
 if __name__ == '__main__':
     test_rbm()
