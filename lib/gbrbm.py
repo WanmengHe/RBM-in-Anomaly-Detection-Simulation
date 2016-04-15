cc68fbaa41b72acd57082d0c15c723cac6b7a6c4
meowoodie <woodielove@163.com>
2016-04-15 22:12:38 +0800

    add the snippet of code about generating. And debug has passed.

--- lib/gbrbm.py
+++ lib/gbrbm.py
@@ -49,7 +49,7 @@ class GBRBM(RBM):
         return [pre_sigmoid_v1, v1_mean, v1_sample]
 
 
-def test_gbrbm(learning_rate=0.1, training_epochs=500, batch_size=2, n_hidden=7):
+def test_gbrbm(learning_rate=0.01, training_epochs=10, batch_size=2, n_hidden=7, n_chains=2, n_samples=10):
     """
     Demonstrate how to train and afterwards sample from it using Theano.
 
@@ -75,14 +75,22 @@ def test_gbrbm(learning_rate=0.1, training_epochs=500, batch_size=2, n_hidden=7)
                [333, 8, 110],
                [344, 6, 100],
                [355, 4, 110]]
-
     datasets = numpy.array(dataset)
     datasets = datasets.astype(theano.config.floatX)
-    # datasets = load_data(dataset)
-
     train_set_x = datasets
     train_set_x = theano.shared(train_set_x)
-    # test_set_x, test_set_y = datasets[2]
+
+
+    testset = [[1, 2, 3],
+               [3, 3, 4],
+               [1, 2, 3],
+               [2, 3, 4],
+               [4, 5, 6],
+               [1, 2, 3]]
+    testsets = numpy.array(testset)
+    testsets = datasets.astype(theano.config.floatX)
+    test_set_x = testsets
+    test_set_x = theano.shared(test_set_x)
 
     # compute number of minibatches for training, validation and testing
     n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
@@ -151,6 +159,74 @@ def test_gbrbm(learning_rate=0.1, training_epochs=500, batch_size=2, n_hidden=7)
     pretraining_time = (end_time - start_time) - plotting_time
 
     print ('Training took %f minutes' % (pretraining_time / 60.))
+
+
+
+
+    number_of_test_samples = test_set_x.get_value(borrow=True).shape[0]
+
+    # pick random test examples, with which to initialize the persistent chain
+    test_idx = rng.randint(number_of_test_samples - n_chains)
+    persistent_vis_chain = theano.shared(
+        numpy.asarray(
+            test_set_x.get_value(borrow=True)[test_idx:test_idx + n_chains],
+            dtype=theano.config.floatX
+        )
+    )
+
+    plot_every = 100
+    # define one step of Gibbs sampling (mf = mean-field) define a
+    # function that does `plot_every` steps before returning the
+    # sample for plotting
+    (
+        [
+            presig_hids,
+            hid_mfs,
+            hid_samples,
+            presig_vis,
+            vis_mfs,
+            vis_samples
+        ],
+        updates
+    ) = theano.scan(
+        gbrbm.gibbs_vhv,
+        outputs_info=[None, None, None, None, None, persistent_vis_chain],
+        n_steps=plot_every
+    )
+
+    # add to updates the shared variable that takes care of our persistent
+    # chain :.
+    updates.update({persistent_vis_chain: vis_samples[-1]})
+    # construct the function that implements our persistent chain.
+    # we generate the "mean field" activations for plotting and the actual
+    # samples for reinitializing the state of our persistent chain
+    sample_fn = theano.function(
+        [],
+        [
+            vis_mfs[-1],
+            vis_samples[-1]
+        ],
+        updates=updates,
+        name='sample_fn'
+    )
+
+    # image_data = numpy.zeros(
+    #     (29 * n_samples + 1, 29 * n_chains - 1),
+    #     dtype='uint8'
+    # )
+    for idx in xrange(n_samples):
+        # generate `plot_every` intermediate samples that we discard,
+        # because successive samples in the chain are too correlated
+        vis_mf, vis_sample = sample_fn()
+        print ' ... plotting sample ', idx
+        # image_data[29 * idx:29 * idx + 28, :] = tile_raster_images(
+        #     X=vis_mf,
+        #     img_shape=(28, 28),
+        #     tile_shape=(1, n_chains),
+        #     tile_spacing=(1, 1)
+        # )
+        # print vis_mf
+        print vis_sample
 
 
 if __name__ == '__main__':
